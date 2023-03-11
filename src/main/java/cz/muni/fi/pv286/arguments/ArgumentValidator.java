a3d136b425783feb159a138d0bc349cd2b39b4c8
Veronika Hanulíková <xhanulik@fi.muni.cz>
2023-03-11 09:00:17 +0100

    Add argument parser prototype

---
+++ src/main/java/cz/muni/fi/pv286/arguments/ArgumentValidator.java
@@ -0,0 +1,105 @@
+package cz.muni.fi.pv286.arguments;
+
+import cz.muni.fi.pv286.arguments.values.FileType;
+import cz.muni.fi.pv286.arguments.values.Option;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArgumentValidator {
+
+    /*  In case contradictory options are provided, use the latter. In case
+     *  multiple options can be combined, apply all of them.
+     */
+
+    private static void validateInputFormat(ProgramArguments arguments) throws InvalidArgumentsException {
+        switch (arguments.getInputFormat()) {
+            case NONE:
+                throw new InvalidArgumentsException("No input format given");
+            case BYTES:
+                if (!arguments.getInputOption().isEmpty()) {
+                    throw new InvalidArgumentsException("Bytes format does not support input options");
+                }
+                break;
+            case HEX:
+                if (!arguments.getInputOption().isEmpty()) {
+                    throw new InvalidArgumentsException("Hex format does not support input options");
+                }
+                break;
+            case INT:
+                Option lastInt = arguments.getInputOption().get(arguments.getInputOption().size() - 1);
+                if (!lastInt.equals(Option.BIG) && !lastInt.equals(Option.LITTLE)) {
+                    throw new InvalidArgumentsException("Invalid option for integer input format");
+                }
+                arguments.setInputOption(new ArrayList<>(List.of(lastInt)));
+                break;
+            case BITS:
+                Option lastBits = arguments.getInputOption().get(arguments.getInputOption().size() - 1);
+                if (!lastBits.equals(Option.LEFT) && !lastBits.equals(Option.RIGHT)) {
+                    throw new InvalidArgumentsException("Invalid option for bits input format");
+                }
+                arguments.setInputOption(new ArrayList<>(List.of(lastBits)));
+                break;
+            case ARRAY:
+                // TODO: Validate two values - format and brackets
+                break;
+            default:
+                throw new InvalidArgumentsException("Undefined input format");
+        }
+    }
+
+    private static void validateOutputFormat(ProgramArguments arguments) throws InvalidArgumentsException {
+        switch (arguments.getOutputFormat()) {
+            case NONE:
+                throw new InvalidArgumentsException("No output format given");
+            case BYTES:
+                if (!arguments.getOutputOption().isEmpty()) {
+                    throw new InvalidArgumentsException("Bytes format does not support output options");
+                }
+                break;
+            case HEX:
+                if (!arguments.getOutputOption().isEmpty()) {
+                    throw new InvalidArgumentsException("Hex format does not support output options");
+                }
+                break;
+            case INT:
+                Option lastInt = arguments.getOutputOption().get(arguments.getOutputOption().size() - 1);
+                if (!lastInt.equals(Option.BIG) && !lastInt.equals(Option.LITTLE)) {
+                    throw new InvalidArgumentsException("Invalid option for integer output format");
+                }
+                arguments.setOutputOption(new ArrayList<>(List.of(lastInt)));
+                break;
+            case BITS:
+                Option lastBits = arguments.getOutputOption().get(arguments.getOutputOption().size() - 1);
+                if (!lastBits.equals(Option.LEFT) && !lastBits.equals(Option.RIGHT)) {
+                    throw new InvalidArgumentsException("Invalid option for bits output format");
+                }
+                arguments.setOutputOption(new ArrayList<>(List.of(lastBits)));
+                break;
+            case ARRAY:
+                // TODO: Validate two values - format and brackets
+                break;
+            default:
+                throw new InvalidArgumentsException("Undefined output format");
+        }
+    }
+
+    public static void validateArguments(ProgramArguments arguments) throws InvalidArgumentsException {
+        if (arguments.getHelp()) {
+            return;
+        }
+
+        validateInputFormat(arguments);
+        validateOutputFormat(arguments);
+
+        if (arguments.getInputFileType().equals(FileType.FILE)
+            && arguments.getInputFileName().isEmpty()) {
+            throw new InvalidArgumentsException("Input file name is missing");
+        }
+
+        if (arguments.getOutputFileType().equals(FileType.FILE)
+                && arguments.getOutputFileName().isEmpty()) {
+            throw new InvalidArgumentsException("Output file name is missing");
+        }
+    }
+}
---
+++ src/main/java/cz/muni/fi/pv286/arguments/values/Option.java
@@ -0,0 +1,15 @@
+package cz.muni.fi.pv286.arguments.values;
+
+public enum Option {
+    NONE,
+    BIG,
+    LITTLE,
+    LEFT,
+    RIGHT,
+    HEX,
+    DEC,
+    CHAR,
+    CURLY,
+    SQUARE,
+    REGULAR
+}
