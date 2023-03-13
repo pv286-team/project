892e4112e4bd7b1c48dc34aa5471d750e267e559
Veronika Hanulíková <xhanulik@fi.muni.cz>
2023-03-13 21:33:15 +0100

    Fix flushing of bits

--- src/main/java/cz/muni/fi/pv286/parser/input/PanbyteBitInput.java
+++ src/main/java/cz/muni/fi/pv286/parser/input/PanbyteBitInput.java
@@ -14,6 +14,7 @@ public class PanbyteBitInput extends PanbyteInput {
     private static final Byte[] allowedCharacters = {'0', '1'};
     /** Initialized in constructor as set for faster searching in allowed LOWERCASE characters */
     private final Set<Byte> allowedCharactersSet = new HashSet<>();
+    private static final int highestBitIndex = 7;
 
     /** While reading bits, they might be aligned from left or right
      *  This can be adjusted after reading all bits from input */
@@ -36,7 +37,7 @@ public class PanbyteBitInput extends PanbyteInput {
 
         Byte readByte;
         byte currentByte = 0;
-        int numberOfBits = 7;
+        int numberOfBits = highestBitIndex;
 
         // read unparsed bits while they are available
         while ((readByte = this.popUnparsedByte()) != null) {
@@ -46,20 +47,20 @@ public class PanbyteBitInput extends PanbyteInput {
             numberOfBits--;
             bitsNumber++;
             if (numberOfBits == -1) {
-                unalignedParsedBytes.add(currentByte);
-                numberOfBits = 7;
+                addSingleByte(currentByte);
+                numberOfBits = highestBitIndex;
                 currentByte = 0;
             }
         }
 
         // if there is less than 8 bits, add them as only one byte
         if (numberOfBits != 7) {
-            unalignedParsedBytes.add(currentByte);
+             addSingleByte(currentByte);
         }
-
-        // bit are now RIGHT padded, check & fix it for LEFT padding if needed
-        this.parsedBytes.addAll(this.alignBytes(this.padding));
-        this.flush();
+        // the right padding is the native way how to store bits, no need for aligning
+        if (padding == Option.RIGHT_PAD) {
+            this.flush();
+        }
     }
 
     @Override
@@ -71,13 +72,29 @@ public class PanbyteBitInput extends PanbyteInput {
      * Assert that all left bytes in the unparsed buffer are whitespace
      */
     @Override
-    public void parserFinalize() {
-        // check if everything was already parsed and nothing more is available
-        if (this.popUnparsedByte() != null) {
+    public void parserFinalize() throws IOException {
+        // for right padding there should be no more left bytes
+        if (padding == Option.RIGHT_PAD && this.popUnparsedByte() != null) {
             throw new IllegalArgumentException("More input is expected");
+        }
+        // for left padding, we need to read all data from input and then align
+        if (padding == Option.LEFT_PAD) {
+            this.parsedBytes.addAll(alignBytesLeft());
+            this.flush();
         }
 
         super.parserFinalize();
+    }
+
+    /**
+     * Add processed byte either directly to output or to the temporal auxiliary buffer
+     */
+    private void addSingleByte(byte current) {
+        if (padding == Option.LEFT_PAD) {
+            unalignedParsedBytes.add(current);
+        } else {
+            this.parsedBytes.add(current);
+        }
     }
 
     /**
@@ -114,7 +131,7 @@ public class PanbyteBitInput extends PanbyteInput {
     private byte shift(byte in, int shift, boolean direction) {
         byte out = 0;
         // go through all bits in input byte, check where 1 is and insert it shifted to the output byte
-        for (int i = 7; i >= 0; i--) {
+        for (int i = highestBitIndex; i >= 0; i--) {
             int mask = 1 << i;
             if (((int) in & (1 << i)) != 0) {
                 if (direction) {
@@ -128,39 +145,29 @@ public class PanbyteBitInput extends PanbyteInput {
     }
 
     /**
-     * Takes unaligned parsed bit and aligns them according to the intpu option
+     * Takes unaligned parsed bit and aligns them according to the input option
      * @return padded bits from left/right
      */
-    private ArrayList<Byte> alignBytes(Option padding) {
+    private ArrayList<Byte> alignBytesLeft() {
         ArrayList<Byte> alignedBytes = new ArrayList<>();
-        switch (padding) {
-            case LEFT_PAD: {
-                if (bitsNumber == 0 || bitsNumber % 8 == 0) {
-                    // We do not have to add padding
-                    alignedBytes.addAll(unalignedParsedBytes);
-                    break;
-                }
-                // Get size of the shift and pad the first byte
-                int shift = 8 - (bitsNumber % 8);
-                byte b = unalignedParsedBytes.get(0);
-                byte shifted = shift(b, shift, true);
-                alignedBytes.add(shifted);
+        if (bitsNumber == 0 || bitsNumber % 8 == 0) {
+            // We do not have to add padding
+            alignedBytes.addAll(unalignedParsedBytes);
+            return alignedBytes;
+        }
 
-                // Then, get second part of the current byte and first part of next byte
-                for (int i = 0; i < unalignedParsedBytes.size() - 1; i++) {
-                    byte currentByte = 0;
-                    currentByte |= shift(unalignedParsedBytes.get(i), 8 - shift, false);
-                    currentByte |= shift(unalignedParsedBytes.get(i + 1), shift, true);
-                    alignedBytes.add(currentByte);
-                }
-                break;
-            }
-            case RIGHT_PAD:
-                // Right padding is already done
-                alignedBytes.addAll(unalignedParsedBytes);
-                break;
-            default:
-                break;
+        // Get size of the shift and pad the first byte
+        int shift = 8 - (bitsNumber % 8);
+        byte b = unalignedParsedBytes.get(0);
+        byte shifted = shift(b, shift, true);
+        alignedBytes.add(shifted);
+
+        // Then, get second part of the current byte and first part of next byte
+        for (int i = 0; i < unalignedParsedBytes.size() - 1; i++) {
+            byte currentByte = 0;
+            currentByte |= shift(unalignedParsedBytes.get(i), 8 - shift, false);
+            currentByte |= shift(unalignedParsedBytes.get(i + 1), shift, true);
+            alignedBytes.add(currentByte);
         }
         return alignedBytes;
     }
