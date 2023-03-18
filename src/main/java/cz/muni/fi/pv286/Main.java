6bc5079b8d20882ba5e42e7cbe14b80d2a9697dd
Lubomir Hrbacek <lubomirhrbacek@seznam.cz>
2023-03-18 18:47:18 +0100

    fix(main) fix getting option in Main when INT parser

--- src/main/java/cz/muni/fi/pv286/Main.java
+++ src/main/java/cz/muni/fi/pv286/Main.java
@@ -64,7 +64,7 @@ public class Main {
                 output = new PanbyteBitOutput(outputStream);
                 break;
             case INT:
-                output = new PanbyteIntOutput(outputStream, arguments.getOutputOption().get(0));
+                output = new PanbyteIntOutput(outputStream, arguments.getOutputOption());
                 break;
             default:
                 System.out.print("Not implemented yet.");
@@ -87,7 +87,7 @@ public class Main {
                 input = new PanbyteBitInput(output, arguments.getInputOption());
                 break;
             case INT:
-                input = new PanbyteIntInput(output, arguments.getInputOption().get(0));
+                input = new PanbyteIntInput(output, arguments.getInputOption());
                 break;
             default:
                 System.out.print("Not implemented yet.");
