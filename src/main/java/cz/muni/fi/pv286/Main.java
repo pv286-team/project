e9e61d36bdc4486513d8a7c50da77b3b8900e81f
Veronika Hanulíková <xhanulik@fi.muni.cz>
2023-04-01 16:59:17 +0200

    feat(array): parse output

--- src/main/java/cz/muni/fi/pv286/Main.java
+++ src/main/java/cz/muni/fi/pv286/Main.java
@@ -8,12 +8,8 @@ import cz.muni.fi.pv286.parser.input.PanbyteHexInput;
 import cz.muni.fi.pv286.parser.input.PanbyteInput;
 import cz.muni.fi.pv286.parser.input.PanbyteIntInput;
 import cz.muni.fi.pv286.parser.input.PanbyteRawInput;
-import cz.muni.fi.pv286.parser.output.PanbyteHexOutput;
+import cz.muni.fi.pv286.parser.output.*;
 import cz.muni.fi.pv286.parser.input.PanbyteBitInput;
-import cz.muni.fi.pv286.parser.output.PanbyteBitOutput;
-import cz.muni.fi.pv286.parser.output.PanbyteOutput;
-import cz.muni.fi.pv286.parser.output.PanbyteIntOutput;
-import cz.muni.fi.pv286.parser.output.PanbyteRawOutput;
 
 import java.io.*;
 import java.io.IOException;
@@ -66,6 +62,9 @@ public class Main {
                 break;
             case INT:
                 output = new PanbyteIntOutput(outputStream, arguments.getOutputOption());
+                break;
+            case ARRAY:
+                output = new PanbyteArrayOutput(outputStream, arguments.getOutputOption(), arguments.getOutputBrackets());
                 break;
             default:
                 System.out.print("Not implemented yet.");
